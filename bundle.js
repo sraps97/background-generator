234639a857700f1ac98c60f691ecad1988f6eb29
Orapeleng Ditsoane <bebetoditsoane@gmail.com>
2023-09-03 09:49:30 +0200

    added the npm files

--- bundle.js
+++ bundle.js
@@ -17217,7 +17217,8 @@ var css=document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-
+var array = [1,2,3,4,5,6,7,8,9]
+console.log("answer:",_.without(array,3))
 
  
 
